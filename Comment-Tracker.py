d7eec13ce08c79f1996f1450035b4223a72b0f65
zEleceed <133176949+zEleceed@users.noreply.github.com>
2024-05-08 09:45:13 +0900

    Sort, create and read files functions

--- Comment-Tracker.py
+++ Comment-Tracker.py
@@ -2,8 +2,33 @@ from pathlib import Path
 from datetime import datetime
 
 
-def append_comments(file_path: Path, comment:str):
+def append_comments(file_path: Path, comment: str):
     # Function to append a comment with current date/time to a students .txt
-    current_date = datetime.now().strftime("%Y/%m/%d")
+    current_date = datetime.now().strftime("%Y/%m/%d")  # Formatting with Year/Month/Day
     with open(file_path, "a") as file:
         file.write(f"{current_date}: {comment}\n")
+
+
+def create_student_file(file_path: Path):
+    # Create an empty file for the student.
+    file_path.touch()
+
+
+def read_file(file_path: Path):
+    # If needed, read and display all comments from the file.
+    with open(file_path, "r") as file:
+        print(f'All saved comments:')
+        print(file.read())
+
+
+def get_all_students_files(directory: Path):
+    # This function will return all student.txt in a directory to view.
+    # It will return a dictionary, so it will be easier to access them later.
+    txt_files = sorted(directory.glob(".txt"), key=lambda f: f.name)  # Sort students alphabetically
+    students_dictionary = {}
+    index = 1
+
+    for student in txt_files:
+        students_dictionary[index] = student
+        index += 1
+    return students_dictionary
